6892fed723e049636382c46193d073ffb3feb76f
Sean Rahman <35696076+SpaceXElaborator@users.noreply.github.com>
2025-12-10 21:51:18 +0100

    Import fix
    Fixed the import for ShardProviderUICreation being moved in Shard from shard.shards to shard.

--- src/main/java/com/rahman/shard/OpenStackShard/ui/ObtainFlavors.java
+++ src/main/java/com/rahman/shard/OpenStackShard/ui/ObtainFlavors.java
@@ -8,8 +8,8 @@ import org.openstack4j.api.OSClient.OSClientV3;
 import org.openstack4j.model.compute.Flavor;
 import org.openstack4j.openstack.OSFactory;
 
-import com.rahman.arctic.shard.ShardProviderUICreation;
 import com.rahman.arctic.shard.objects.providers.ProviderFlavor;
+import com.rahman.arctic.shard.shards.ShardProviderUICreation;
 import com.rahman.arctic.shard.shards.UIField;
 
 public class ObtainFlavors extends ShardProviderUICreation<ProviderFlavor> {
